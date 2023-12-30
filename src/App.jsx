137738b198e1946026cde3ace7cc50767b9dc60f
Jonathan Houtmeyers <Houtmeyers Jonathan>
2023-12-30 16:27:34 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+import "./style.css";
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
+);
+
+const App = () => {
+  const [cadeaus, setCadeaus] = useState([]);
+  const [newCadeau, setNewCadeau] = useState("");
+  const getCadeaus = async () => {
+    const { data } = await supabase
+      .from("cadeaus")
+      .select()
+      .order("created_at", { ascending: false });
+    setCadeaus(data);
+  };
+
+  const toggleCheck = async (id, checked) => {
+    const { error } = await supabase
+      .from("cadeaus")
+      .update({ checked })
+      .eq("id", id);
+    getCadeaus();
+  };
+
+  const deleteCadeau = async (id) => {
+    await supabase.from("cadeaus").delete().eq("id", id);
+    getCadeaus();
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!newCadeau) {
+      return;
+    } else {
+      await supabase.from("cadeaus").insert({
+        gift: newCadeau,
+      });
+    }
+
+    setNewCadeau("");
+    getCadeaus();
+  };
+
+  useEffect(() => {
+    getCadeaus();
+  }, []);
+
+  return (
+    <div className="container">
+      <div>
+        <h1>Lijst met gewenste cadeaus</h1>
+        <form className="form-flex" action="" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={newCadeau}
+            placeholder="Gewenst cadeau..."
+            onChange={(e) => setNewCadeau(e.target.value)}
+          />
+          <button type="submit">Add cadeau</button>
+        </form>
+        <ul>
+          {cadeaus.map(({ id, gift, checked }) => (
+            <div className="flex-data">
+              <li className={checked ? "checked" : ""} key={id}>
+                {gift}
+                <button onClick={() => toggleCheck(id, !checked)}>
+                  {checked ? "uncheck" : "check"}
+                </button>
+                <button onClick={() => deleteCadeau(id)}>Delete</button>
+              </li>
+            </div>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
